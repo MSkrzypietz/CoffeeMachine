5dee0f315c65d34306dfc11d9bba5e8d47a80f94
Michael Skrzypietz <michael.s97@web.de>
2018-02-09 22:38:00 +0100

    finished grinding coffee beans method

--- src/base/CoffeeMachine.java
+++ src/base/CoffeeMachine.java
@@ -1,6 +1,7 @@
 package base;
 
 import containers.*;
+import observers.ICoffeeFlourLevelListener;
 
 public class CoffeeMachine {
 
@@ -10,21 +11,28 @@ public class CoffeeMachine {
     private Container brewContainer = new BrewContainer();
     private Container wasteContainer = new WasteContainer();
 
-    public CoffeeMachine() {}
+    public CoffeeMachine() {
+        coffeeBeanContainer.fillUp();
+        waterContainer.fillUp();
+        ((CoffeeFlourContainer) coffeeFlourContainer).getCoffeeFlourLevelObserver().addListener((ICoffeeFlourLevelListener) waterContainer);
+    }
 
     public void brewCoffee() {
         grindCoffeeBeans(10);
     }
 
     private void grindCoffeeBeans(int amount) {
+        coffeeBeanContainer.printContainer();
+        coffeeFlourContainer.printContainer();
         for (int i = 0; i < amount; i++) {
-            coffeeBeanContainer.printContainer();
-            coffeeFlourContainer.printContainer();
             System.out.println("Grinding coffee beans...");
             coffeeBeanContainer.remove(1);
             sleepThread(200);
             coffeeFlourContainer.add(5);
+            ((CoffeeFlourContainer) coffeeFlourContainer).checkIfFullToHeatWater();
         }
+        coffeeBeanContainer.printContainer();
+        coffeeFlourContainer.printContainer();
     }
 
     private void sleepThread(int duration) {
