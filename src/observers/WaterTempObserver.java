49476d57b3836f027024fe8dcf0e99785fe516c6
Michael Skrzypietz <michael.s97@web.de>
2018-02-10 03:19:26 +0100

    added observer for when water is heated

---
+++ src/observers/WaterTempObserver.java
@@ -0,0 +1,24 @@
+package observers;
+
+import java.util.ArrayList;
+
+public class WaterTempObserver {
+
+    private ArrayList<IWaterTempListener> listeners = new ArrayList<>();
+
+    public WaterTempObserver() {}
+
+    public void waterIsHeated() {
+        for (IWaterTempListener listener : listeners)
+            listener.waterIsHeated();
+    }
+
+    public void addListener(IWaterTempListener listener) {
+        this.listeners.add(listener);
+    }
+
+    public void remoteListener(IWaterTempListener listener) {
+        this.listeners.remove(listener);
+    }
+
+}
